c47c3e3ba7df4d64d6975b96f6d985617bc800ad
CalvinPedigo <159497880+CalvinPedigo@users.noreply.github.com>
2024-05-13 16:23:07 -0600

    fixed how the tables were made, and also made them in the database (irrelevant to github). Must import csv files next

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE contacts (
     contact_id INT PRIMARY KEY NOT NULL,
     first_name VARCHAR NOT NULL,
 	last_name VARCHAR NOT NULL,
-	email VARCHAR NOT NULL,
+	email VARCHAR NOT NULL
 );
 
 CREATE TABLE category (
@@ -17,7 +17,8 @@ CREATE TABLE subcategory (
 
 CREATE TABLE campaign (
 	cf_id INT PRIMARY KEY NOT NULL,
-	FOREIGN KEY (contact_id) REFERENCES contacts(contact_id)
+	contact_id INT NOT NULL,
+	FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
 	company_name VARCHAR NOT NULL,
 	description VARCHAR NOT NULL,
 	goal DECIMAL(8, 1) NOT NULL,
@@ -28,6 +29,13 @@ CREATE TABLE campaign (
 	currency VARCHAR(4) NOT NULL,
 	launch_date DATE NOT NULL,
 	end_date DATE NOT NULL,
+	category_id VARCHAR NOT NULL,
 	FOREIGN KEY (category_id) REFERENCES category(category_id),
+	subcategory_id VARCHAR NOT NULL,
 	FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
 );
+
+SELECT * FROM contacts;
+SELECT * FROM category;
+SELECT * FROM subcategory;
+SELECT * FROM campaign;
